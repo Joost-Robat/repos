a22a2a6163dc91b0173078e3cf98f3d4c275b06a
Joost-Robat <30769@ma-web.nl>
2021-10-15 11:05:14 +0200

    Inheritance part 2 les gedaan, ik heb nog een error maar het ging niet zo goed en snap het nog niet helemaal het is nog een beetje verwarrend maar ik vraag de volgeden les wel even voor hulp want het was toch maar een extra opdracht.

---
+++ Abstract Inheritance/Abstract Inheritance/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Abstract_Inheritance
+{
+    abstract class Equipment
+    {
+        public abstract void Equip();
+
+    }
+    class Armor : Equipment
+    {
+        public override void Equip()
+        {
+            
+        }
+    }
+    class player
+    {
+        public float playerWallet;
+        float playerHealth;
+        public player(float wallet, float health)
+        {
+            playerHealth = health;
+            playerWallet = wallet;
+        }
+    }
+    abstract class pickUp
+    {
+        public abstract void pickSomethingUp(player person);
+    }
+    class Money : pickUp
+    {
+        float moneyPickedUp;
+        public override void pickSomethingUp(player person)
+        {
+            person.playerWallet += moneyPickedUp;
+        }
+        Money(float money)
+        {
+            moneyPickedUp = money;
+            pickSomethingUp();
+        }
+    }
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            player person = new player(20f, 100f);
+        }
+    }
+}
